36f9e0ebf3e1f49777da3d226ca03314d8109991
Andrew :) <Acradawg@gmail.com>
2025-07-07 13:43:38 -0500

    animated favicon(real)

--- src/scripts/animate.ts
+++ src/scripts/animate.ts
@@ -3,11 +3,11 @@ let src = "src/assets/emerald1.png";
 
 export function animateFavicon() {
     if (turn % 3 == 1) {
-        src = "src/assets/emerald2.png";
+        src = "/src/assets/emerald2.png";
     } else if (turn % 3 == 2) {
-        src = "src/assets/emerald3.png";
+        src = "/src/assets/emerald3.png";
     } else {
-        src = "src/assets/emerald1.png";
+        src = "/src/assets/emerald1.png";
         turn = 0;
     }
 
